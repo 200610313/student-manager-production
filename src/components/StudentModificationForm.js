39e6180451f6900e722c53c882ca5cc68e2e94c2
angelu <200610313@my.xu.edu.ph>
2020-04-03 11:38:34 +0800

    Created StudentModificationForm component

---
+++ src/components/StudentModificationForm.js
@@ -0,0 +1,18 @@
+class StudentModificationForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            fN: '',
+            lN: '',
+            nG: ''
+         };
+    }
+    render() {
+        // if (this.props.)
+        return (
+            null
+        );
+    }
+}
+
+export default StudentModificationForm;
