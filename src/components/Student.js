f0cddc55054bd9c00f8911afefe890b1219d410f
angelu <200610313@my.xu.edu.ph>
2020-04-03 11:37:51 +0800

    Converted Student functional component to class component

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,65 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
+class Student extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { modifiedBtnClicked: false };
+  }
+  render() {
+    const nGrade = //  Letter grade translation
+        this.props.nGrade < 50
+          ? 'F'
+          : this.props.nGrade < 60
+          ? 'D'
+          : this.props.nGrade < 68
+          ? 'C'
+          : this.props.nGrade < 76
+          ? 'B-'
+          : this.props.nGrade < 84
+          ? 'B'
+          : this.props.nGrade < 92
+          ? 'A-'
+          : 'A',
+      id = this.props.id,
+      fName = this.props.fName,
+      lName = this.props.lName,
+      numGrade = this.props.nGrade;
 
-// Formats the render for each student that appears in StudentList, StudentsPassedList and StudentsFailedList
-const Student = (props) => {
-  if (props.unsorted)
-    //Display with buttons | used by studentlist
-    return (
-      <tr>
-        <th scope='row'>{props.id}</th>
-        <td>{props.fName}</td>
-        <td>{props.lName}</td>
-        <td>{props.nGrade}</td>
-        <td>
-          {/* Letter grade translation */}
-          {props.nGrade < 50
-            ? 'F'
-            : props.nGrade < 60
-            ? 'D'
-            : props.nGrade < 68
-            ? 'C'
-            : props.nGrade < 76
-            ? 'B-'
-            : props.nGrade < 84
-            ? 'B'
-            : props.nGrade < 92
-            ? 'A-'
-            : 'A'}
-        </td>
-        <td><button onClick = {props.handleDelete.bind(this,props.id)} type="button" className="btn btn-danger btn-block">Delete</button></td>
-        <td><button onClick = {props.handleModify.bind(this,[props.id,props.fName,props.lName,props.nGrade])} type="button" className="btn btn-info btn-block">Modify</button></td>
-      </tr>
+    let buffer = []; //  Contains the proper JSX according to props.unsorted
+    buffer.push(  // Push basic data
+      // React.Fragment is used to prevent wrapping with <div>
+      <React.Fragment>
+        <th scope='row'>{id}</th>
+        <td>{fName}</td>
+        <td>{lName}</td>
+        <td>{numGrade}</td>
+        <td>{nGrade}</td>
+      </React.Fragment>
     );
-  
-  else //Display without buttons | used by studentlist
-    return (
-      <tr>
-        <th scope='row'>{props.id}</th>
-        <td>{props.fName}</td>
-        <td>{props.lName}</td>
-        <td>{props.nGrade}</td>
-        <td>
-          {/* Letter grade translation */}
-          {props.nGrade < 50
-            ? 'F'
-            : props.nGrade < 60
-            ? 'D'
-            : props.nGrade < 68
-            ? 'C'
-            : props.nGrade < 76
-            ? 'B-'
-            : props.nGrade < 84
-            ? 'B'
-            : props.nGrade < 92
-            ? 'A-'
-            : 'A'}
-        </td>
-      </tr>
-    );
-    
-};
+    if (this.props.unsorted) {  // If received StudentList as element, we push delete and modify buttons
+      buffer.push(
+        <React.Fragment>
+          <td>
+            <button
+              onClick={this.props.handleDelete.bind(this, id)}
+              type='button'
+              className='btn btn-danger btn-block'
+            >
+              Delete
+            </button>
+          </td>
+          <td>
+            <button
+              onClick={this.props.handleModify.bind(this, [
+                id,
+                fName,
+                lName,
+                numGrade
+              ])}
+              type='button'
+              className='btn btn-info btn-block'
+            >
+              Modify
+            </button>
+          </td>
+        </React.Fragment>
+      );
+    }
+    return(<tr>{buffer}</tr>)
+  }
+}
 Student.defaultProps = {
   unsorted: false
 };
-
 export default Student;
