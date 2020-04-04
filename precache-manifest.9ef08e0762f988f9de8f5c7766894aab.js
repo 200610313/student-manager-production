d1a676849736766668707c8fbfa7aaac68c687b6
angelu <200610313@my.xu.edu.ph>
2020-04-04 13:29:00 +0800

    Updates

---
+++ precache-manifest.9ef08e0762f988f9de8f5c7766894aab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b2d187848906782e5ad7fd2e158036",
+    "url": "/student-manager-production/index.html"
+  },
+  {
+    "revision": "90ed18706db364ee0dca",
+    "url": "/student-manager-production/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "856952c02cce466a7fc2",
+    "url": "/student-manager-production/static/js/2.eb231aaf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/student-manager-production/static/js/2.eb231aaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90ed18706db364ee0dca",
+    "url": "/student-manager-production/static/js/main.4a53acd1.chunk.js"
+  },
+  {
+    "revision": "5f7613ac20091c61343a",
+    "url": "/student-manager-production/static/js/runtime-main.940d3e00.js"
+  }
+]);
