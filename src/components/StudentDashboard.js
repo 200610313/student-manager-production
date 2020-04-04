d43d2c93664b630ae6932577b337a7139533af26
angelu <200610313@my.xu.edu.ph>
2020-04-04 10:05:56 +0800

    added states pertaining to the student info to be modified

--- src/components/StudentDashboard.js
+++ src/components/StudentDashboard.js
@@ -9,6 +9,8 @@ class StudentDashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: -1, //  ID of the student to be modified
+      studToModify: null, // Only used as placeholder for the Modify modal form
       toggleForm: false, //  true to show form when modify btn is clicked
       students: [
         //  Holds the current list of students
@@ -16,37 +18,37 @@ class StudentDashboard extends Component {
           id: 1,
           fName: 'Angelu Ferdinand',
           lName: 'Garcia',
-          nGrade: 100
+          nGrade: 100,
         },
         {
           id: 2,
           fName: 'Lyra Angela',
           lName: 'Manseguiao',
-          nGrade: 91
+          nGrade: 91,
         },
         {
           id: 3,
           fName: 'John',
           lName: 'Doe',
-          nGrade: 49
+          nGrade: 49,
         },
         {
           id: 4,
           fName: 'Jack',
           lName: 'Black',
-          nGrade: 32
+          nGrade: 32,
         },
         {
           id: 5,
           fName: 'Fun',
           lName: 'Do',
-          nGrade: 81
-        }
-      ]
+          nGrade: 81,
+        },
+      ],
     };
     this.handleSubmit = this.handleSubmit.bind(this); // Bind submit btn to this class
     this.handleDelete = this.handleDelete.bind(this);
-    // this.handleModify = this.handleModify.bind(this);
+    this.handleModify = this.handleModify.bind(this);
     this.toggleForm = this.toggleForm.bind(this);
   }
 
@@ -59,11 +61,11 @@ class StudentDashboard extends Component {
       id, // ES6 feature: these attributes are assigned to values from this local method
       fName,
       lName,
-      nGrade
+      nGrade,
     };
     this.setState((state, props) => {
       return {
-        students: [...state.students, studToAdd] // ES6 spread operator | copies by value instead of reference
+        students: [...state.students, studToAdd], // ES6 spread operator (like append) | copies by value instead of reference
       };
     });
   }
@@ -75,29 +77,46 @@ class StudentDashboard extends Component {
 
     this.setState((state, props) => {
       return {
-        students: state.students.filter((stud) => stud.id !== studentID) // if equal to parameter, do not include it in new state
+        students: state.students.filter((stud) => stud.id !== studentID), // if equal to parameter, do not include it in new state
       };
     });
   }
 
   // Handles Student's modify button
   handleModify(modifiedInfo) {
-    const [id, newFN, newLN, newNG] = modifiedInfo;
+    let [newFN, newLN, newNG] = modifiedInfo;
+    
+    // If any inputboxes left blanked, information is unchanged
+    if (newFN == null || newFN.trim() === '') newFN = this.state.studToModify.fName
+    if (newLN == null || newLN.trim() === '') newLN = this.state.studToModify.lName
+    if (newNG <= 0 || newNG === undefined) newNG = this.state.studToModify.nGrade
+    
     this.setState((state, props) => {
       return {
         students: state.students.map((stud) => {
-          if (stud.id === id) {
+          if (stud.id === this.state.id) {
             stud.fName = newFN;
             stud.lName = newLN;
-            stud.lGrade = newNG;
+            stud.nGrade = newNG;
           }
           return stud;
-        })
+        }),
       };
     });
   }
 
-  toggleForm() {
+  // Switch to show form
+  toggleForm(id) {
+    if (id !== undefined) {
+      this.setState((state, props) => {
+        return { id: id };
+      });
+      let object = this.state.students.find((o) => o.id === id); // Getting a reference of the student to modify
+      this.setState((state, props) => {
+        return { studToModify: object };
+      }); //  Updating student to modify
+    }
+
     this.setState((state, props) => {
       return { toggleForm: !state.toggleForm };
     });
@@ -115,8 +134,19 @@ class StudentDashboard extends Component {
             handleDelete={this.handleDelete}
             toggleForm={this.toggleForm}
           />
+
           <Summary students={this.state.students} />
-          <StudentModificationForm toggleForm={this.state.toggleForm} toggle = {this.toggleForm}/>
+          <StudentModificationForm
+            toggleForm={this.state.toggleForm /* current state of show/hide */}
+            studToModify={
+              this.state.studToModify /* passed to be used as placeholder */
+            }
+            toggle={
+              this
+                .toggleForm /*  handler that enables to set show/hide modal form */
+            }
+            handleModify = {this.handleModify}
+          />
         </div>
       </div>
     );
